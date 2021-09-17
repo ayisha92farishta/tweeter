937c55ea217b5627ea7099873f4631e0b8b2a2de
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-09-17 18:18:57 +0000

    Add counter to text-Area

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  console.log( "ready!" );
+
+ 
+  $('#tweet-text').on('input', (event) => {
+    const inputLength = event.target.value.length
+    const actualLength = 140 - inputLength
+    const $counter = $('.counter')
+
+    $counter.text(actualLength)
+
+    if(actualLength < 0) {
+      $counter.css({ 'color': 'red'})
+    } 
+    if(actualLength > 0){
+      $counter.css({ 'color': 'black'})
+    }
+  })
+});
+
+
+

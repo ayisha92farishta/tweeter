961b4a09f178aea7a224d0e025a99d80ec9a854d
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-09-23 21:52:46 +0000

    Add minimum and maximum limit in text area

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": '12 September 2021'
-  }
-]
-
 $(document).ready(() => {
   
 
@@ -93,8 +68,6 @@ const loadTweets = function () {
     }
 });
 }
-
-//renderTweets(loadTweets()) ;
 
 loadTweets()
 
